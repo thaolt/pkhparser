9b331b255b9abd546744c741c6417f33f56086ca
thaolt <thaolt@songphi.com>
2019-06-20 11:50:31 +0700

    first commit

---
+++ pkhparser.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+def parse_hand( input_line )
+    valueref = {
+        "C"  => 0, "D" => 20, "H" => 40, "S" => 60,
+        "T" => 10, "J" => 11, "Q" => 12, "K" => 13, "A" => 14
+    }
+    rank = []
+    pairs = 0
+    three_of_kinds = 0
+    cards = input_line.scan(/([SHDC])(10|[2-9TJQKA])/)
+    
+    if cards.size < 5 
+        return "invalid input" 
+    end
+
+    cards.each{ |c|
+        rank.push( c[1].match(/\d+/)?(c[1].to_i):valueref[c[1]] )
+    }
+    rank = rank.sort
+    idx = 0
+    while idx < 4
+        if rank.rindex(rank[idx]) == idx + 3 then return "4C" end
+        if rank.rindex(rank[idx]) == idx + 2
+            three_of_kinds = 1
+            idx += 3
+            next
+        end
+        if rank.rindex(rank[idx]) == idx + 1
+            pairs += 1
+            idx += 2
+            next
+        end
+        idx += 1
+    end
+
+    ((pairs > 0) and (three_of_kinds > 0)) ? "FH" : (
+        three_of_kinds > 0 ? "3C" : (
+            pairs > 0? (pairs.to_s+"P") : "--" 
+        ) 
+    )
+end
+
+
+ARGF.each do |input|
+    print parse_hand( input ), "\n"
+end
+
